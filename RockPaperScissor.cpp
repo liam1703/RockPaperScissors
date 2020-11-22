57371f6cc975a830f1dfb22481ac13913ffefae2
Liam Bradbury <liam.bradbury98@gmail.com>
2020-11-22 12:10:16 +0000

    Create RockPaperScissor.cpp

---
+++ RockPaperScissor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// functions
+// get user choice
+//create computer choice
+// compare and get winner
+//in main keep score 
+
+
+void PrintIntro()
+{
+    std::cout << "Welcome to Rock Paper Scissors press X to quit otherwise type your choice \n";
+    
+}
+
+void GetUserChoice()
+{
+    string UserChoice;
+    std::cin >> UserChoice;
+    std::cout << "\n";
+
+//now need to check users choice is one of ROCK PAPER SCISSORS
+    
+
+}
+
+
+
+
+
+//std::cout << "The current score is:    \n"
