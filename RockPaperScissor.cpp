da01c696eddb646c5f33981353c0c727121051f3
Liam Bradbury <liam.bradbury98@gmail.com>
2020-11-23 13:53:57 +0000

    Update RockPaperScissor.cpp

--- RockPaperScissor.cpp
+++ RockPaperScissor.cpp
@@ -15,15 +15,42 @@ void PrintIntro()
     
 }
 
-void GetUserChoice()
+int GetUserChoice()
 {
     string UserChoice;
     std::cin >> UserChoice;
     std::cout << "\n";
 
 //now need to check users choice is one of ROCK PAPER SCISSORS
-    
+    int ChoiceNum = validInput(UserChoice);
+    return ChoiceNum;
 
+}
+
+int validInput(string inp)
+{
+    string ValidOpOne = "ROCK";
+    string ValidOpTwo = "PAPER";
+    string validOpThree = "SCISSOTS";
+    if(inp.compare(ValidOpOne)!= 0 || inp.compare(ValidOpTwo)!= 0 || inp.compare(validOpThree)!= 0)
+        return 0;
+    else
+    {
+        if (inp.compare(ValidOpOne) == 0)
+        {
+            return 1;
+        }
+        else if (inp.compare(ValidOpTwo) == 0)
+        {
+            return 2;
+        }
+        else
+        {
+            return 3;
+        }
+
+    }
+    
 }
 
 
